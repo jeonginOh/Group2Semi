dd4f52fb7ec9a3c6b63c66e775621112ce914311
근수 <@DESKTOP-KR7ATAM>
2020-12-11 09:50:08 +0900

    Vo추가

--- src/semiVo/IteminfoVo.java
+++ src/semiVo/IteminfoVo.java
@@ -32,6 +32,92 @@ public class IteminfoVo {
 		this.image = image;
 		this.avail = avail;
 	}
-	
-	
+
+	public int getItemid() {
+		return itemid;
+	}
+
+	public void setItemid(int itemid) {
+		this.itemid = itemid;
+	}
+
+	public String getItemname() {
+		return itemname;
+	}
+
+	public void setItemname(String itemname) {
+		this.itemname = itemname;
+	}
+
+	public int getCatid() {
+		return catid;
+	}
+
+	public void setCatid(int catid) {
+		this.catid = catid;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getFactory() {
+		return factory;
+	}
+
+	public void setFactory(String factory) {
+		this.factory = factory;
+	}
+
+	public String getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public Date getExpire() {
+		return expire;
+	}
+
+	public void setExpire(Date expire) {
+		this.expire = expire;
+	}
+
+	public Date getStoredate() {
+		return storedate;
+	}
+
+	public void setStoredate(Date storedate) {
+		this.storedate = storedate;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public int getAvail() {
+		return avail;
+	}
+
+	public void setAvail(int avail) {
+		this.avail = avail;
+	}
 }
---
+++ src/semiVo/LogisticVo.java
@@ -0,0 +1,70 @@
+package semiVo;
+
+public class LogisticVo {
+	private int logiid;
+	private int memid;
+	private int buyid;
+	private int itemid;
+	private String addr;
+	private String logiinfo;
+	
+	public LogisticVo() {}
+
+	public LogisticVo(int logiid, int memid, int buyid, int itemid, String addr, String logiinfo) {
+		super();
+		this.logiid = logiid;
+		this.memid = memid;
+		this.buyid = buyid;
+		this.itemid = itemid;
+		this.addr = addr;
+		this.logiinfo = logiinfo;
+	}
+
+	public int getLogiid() {
+		return logiid;
+	}
+
+	public void setLogiid(int logiid) {
+		this.logiid = logiid;
+	}
+
+	public int getMemid() {
+		return memid;
+	}
+
+	public void setMemid(int memid) {
+		this.memid = memid;
+	}
+
+	public int getBuyid() {
+		return buyid;
+	}
+
+	public void setBuyid(int buyid) {
+		this.buyid = buyid;
+	}
+
+	public int getItemid() {
+		return itemid;
+	}
+
+	public void setItemid(int itemid) {
+		this.itemid = itemid;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getLogiinfo() {
+		return logiinfo;
+	}
+
+	public void setLogiinfo(String logiinfo) {
+		this.logiinfo = logiinfo;
+	}
+}
