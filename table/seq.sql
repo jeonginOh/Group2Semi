bc52a1b262fa0c559fc03626397ec73ef9666110
tjdwlrwk3123 <tjdwlrwk3123@naver.com>
2020-12-26 14:00:48 +0900

    기홍-시퀀스추가

--- table/seq.sql
+++ table/seq.sql
@@ -4,3 +4,10 @@ drop sequence daily_seq;
 create sequence memberinfo_memid;
 create sequence loginauth_seq;
 create sequence daily_seq;
+
+drop sequence bas_seq;
+drop sequence buyid_seq;
+drop sequence logiid_seq;
+create sequence bas_seq;
+create sequence buyid_seq;
+create sequence logiid_seq;
